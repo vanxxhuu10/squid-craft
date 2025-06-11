55ab645aa726debe468cceabeede51550c6d3d49
Vansh Srivastava <srivastavavansh64@gmail.com>
2025-06-11 16:57:50 +0530

    backend

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ const upload = multer({ dest: 'uploads/' });
 app.use(cors());
 
 // Serve static files from "public" folder
-app.use(express.static(path.join(__dirname, 'index.html')));
+app.use(express.static(path.join(__dirname)));
 
 // Serve index.html on root path
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Screenshot upload and email route
